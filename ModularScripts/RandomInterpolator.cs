eadd488ccd53d85f504eae21c5148afa95e4ac93
Loki McKay <sneyed@gmail.com>
2017-01-07 17:45:10 +1000

    removed scriptloadinginterface

--- ModularScripts/RandomInterpolator.cs
+++ ModularScripts/RandomInterpolator.cs
@@ -4,7 +4,7 @@
 using UnityEngine;
 using System.Collections;
 
-public class RandomInterpolator : MonoBehaviour, ScriptLoadingInterface
+public class RandomInterpolator : MonoBehaviour
 {
 
 	[Header("Main Options")]
